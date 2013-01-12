3177e43a9a9e9cf13a0198c178cd06e553f5c384
Jan Matousek <jan@m6k.cz>
2013-01-12 21:18:53 +0100

    remove trailing whitespace I have my editor doing this automatically.

--- tasks/connect.js
+++ tasks/connect.js
@@ -22,15 +22,15 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('connect', 'Run a simple static connect server till you shut it down.', function() {
     // Don't ever close this task!
     this.async();
-    
+
     var port = this.data.port || 1337;
     var base = path.resolve(this.data.base || '.');
-    
+
     grunt.log.success('Opening server for ' + base+ ' on port ' + port + '.');
     connect(
       connect.static(base),
       connect.directory(base)
     ).listen(port);
-    
+
   });
 };
